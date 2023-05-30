b00c67abf4018d18354bdf867839dae73b4ab8c1
Nihal Sharma <itsnihal.sharma@gmail.com>
2023-05-30 22:23:51 +0530

    added static rootd

--- sparrow/settings.py
+++ sparrow/settings.py
@@ -180,6 +180,7 @@ TIME_ZONE = 'Asia/Kolkata'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
